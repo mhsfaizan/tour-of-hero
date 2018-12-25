c57febcc1b18a80d4ea4c2b9d65273180bbeb3ad
Faizan <mhsfaizan@gmail.com>
2018-12-25 17:36:12 +0530

    second commmit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: "Mohd Aijaz"
+    },
+    {
+        id: 2,
+        name: "Mohd Faizan"
+    },
+    {
+        id: 3,
+        name: "Mohd Rizwan"
+    }
+]
